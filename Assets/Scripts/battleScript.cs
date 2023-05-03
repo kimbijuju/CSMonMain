26c0ccec670513507be46706aade98c00d309ff8
kimbijuju <99450510+kimbijuju@users.noreply.github.com>
2023-05-03 12:19:05 -0700

    Update battleScript.cs

--- Assets/Scripts/battleScript.cs
+++ Assets/Scripts/battleScript.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
+
 public class battleScript : MonoBehaviour
 {
     // Start is called before the first frame update
