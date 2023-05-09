c3fb2e56060a3a8dd1fdb6fa19ae180c9d6e90c3
Tony Wang <tonywang12131213@gmail.com>
2023-05-09 12:01:30 -0700

    lol

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -65,7 +65,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsWalkable(Vector3 targetPos)
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        if (Physics2D.OverlapCircle(targetPos, -0.2f, solidObjectsLayer) != null)
         {
             return false;
         }
