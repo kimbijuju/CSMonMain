5edef0ca070600c1081860cabc407ab64f1efead
EeeeeVAN7836 <116920487+EeeeeVAN7836@users.noreply.github.com>
2023-05-10 10:07:37 -0700

    evan work

---
+++ Assets/Scripts/Inventory/UI/InventoryUI.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] GameObject itemList;
+    [SerializeField] ItemSlotUI itemSlotUI;
+
+    [SerializeField] Image itemIcon;
+    [SerializeField] Text itemDescription;
+
+    [SerializeField] Image upArrow;
+    [SerializeField] Image downArrow;
+
+    int selectedItem = 0;
+
+    const int itemsInViewport = 8;
+
+    List<ItemSlotUI> slotUIList;
+    Inventory inventory;
+    RectTransform itemListRect;
+    private void Awake()
+    {
+        inventory = Inventory.GetInventory();
+        itemListRect = itemList.GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        UpdateItemList();
+    }
+
+    void UpdateItemList()
+    {
+        // Clear all the existing items
+        foreach (Transform child in itemList.transform)
+            Destroy(child.gameObject);
+
+        slotUIList = new List<ItemSlotUI>();
+        foreach (var itemSlot in inventory.Slots)
+        {
+            var slotUIObj = Instantiate(itemSlotUI, itemList.transform);
+            slotUIObj.SetData(itemSlot);
+
+            slotUIList.Add(slotUIObj);
+        }
+
+        UpdateItemSelection();
+    }
+
+    public void HandleUpdate(Action onBack)
+    {
+        int prevSelection = selectedItem;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            ++selectedItem;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            --selectedItem;
+
+        selectedItem = Mathf.Clamp(selectedItem, 0, inventory.Slots.Count - 1);
+
+        if (prevSelection != selectedItem)
+            UpdateItemSelection();
+
+        if (Input.GetKeyDown(KeyCode.X))
+            onBack?.Invoke();
+
+    }
+
+    void UpdateItemSelection()
+    {
+        for (int i = 0; i < slotUIList.Count; i++)
+        {
+            if (i == selectedItem)
+                slotUIList[i].NameText.color = GlobalSettings.i.HighlightedColor;
+            else
+                slotUIList[i].NameText.color = Color.black;
+        }
+
+        var item = inventory.Slots[selectedItem].Item;
+        itemIcon.sprite = item.Icon;
+        itemDescription.text = item.Description;
+
+        HandleScrolling();
+    }
+
+    void HandleScrolling()
+    {
+        float scrollPos = Mathf.Clamp(selectedItem - itemsInViewport/2, 0, selectedItem) * slotUIList[0].Height;
+        itemListRect.localPosition = new Vector2(itemListRect.localPosition.x, scrollPos);
+
+        bool showUpArrow = selectedItem > itemsInViewport / 2;
+        upArrow.gameObject.SetActive(showUpArrow);
+
+        bool showDownArrow = selectedItem + itemsInViewport / 2 < slotUIList.Count;
+        downArrow.gameObject.SetActive(showDownArrow);
+    }
+}
