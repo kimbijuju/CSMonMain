e794fff0c3e145978a949334296b31f7a64c0988
kimbijuju <99450510+kimbijuju@users.noreply.github.com>
2023-05-09 23:39:14 -0700

    bijou work 2.0

--- Assets/Scripts/demoPrepScript.cs
+++ Assets/Scripts/demoPrepScript.cs
@@ -8,16 +8,16 @@ public class demoPrepScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Yunxin.hp)*100, PokeList.Yunxin));
+        PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Daniel.hp)*100, PokeList.Daniel));
         PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Evan.hp)*100, PokeList.Evan));
         PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Bijou.hp)*100, PokeList.Bijou));
         
-        PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Vanessa.hp)*100, PokeList.Vanessa));
+        PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Peter.hp)*100, PokeList.Peter));
         PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Steve.hp)*100, PokeList.Steve));
         PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Derrick.hp)*100, PokeList.Derrick));
         PokeList.ObtainedList.Add(new PokeList.CSMon(100, "", (PokeList.Emily.hp)*100, PokeList.Emily));
-        PokeList.enemyList.Add(new PokeList.CSMon(100, "", (PokeList.Emily.hp)*100, PokeList.Emily));
-        PokeList.enemyList.Add(new PokeList.CSMon(100, "", (PokeList.Nilesh.hp)*100, PokeList.Nilesh));
+        
+        PokeList.enemyList.Add(new PokeList.CSMon(100, "", (PokeList.Grace.hp)*100, PokeList.Grace));
 
         SceneManager.LoadScene("DemoBattleRoom");
     }
