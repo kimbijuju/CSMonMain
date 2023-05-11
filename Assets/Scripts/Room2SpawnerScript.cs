33860e728a29ebc886bc5c67fabb0a443c09735c
Tony Wang <tonywang12131213@gmail.com>
2023-05-11 14:48:22 -0700

    Starting of ROOM 2

--- Assets/Scripts/Room2SpawnerScript.cs
+++ Assets/Scripts/Room2SpawnerScript.cs
@@ -7,25 +7,18 @@ public class Room2SpawnerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    public int timer;
     // Start is called before the first frame update
     void Start()
     {
-        texterScript.currLvlScene="Level2Room";
-        timer=0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer++;
-        if(timer>=600){
-            spawnScript.spawnBattle(5,11, PokeList.Room1CSMon);
-            SceneManager.LoadScene("DemoBattleRoom");
-        }
 
         //if(player.transform.position.x>6)
-            //SceneManager.LoadScene("Level3Room");
+           // SceneManager.LoadScene("Level3Room");
 
     }
 }
